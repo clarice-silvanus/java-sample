317bf7044a9f9ce171ea0ce70da340a62f1479fb
clarice-silvanus <claricesilvanus@gmail.com>
2025-05-14 13:23:30 +0700

    java programming

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {  
+  public int x = 7;
+  public int y = 19;
+
+  public static void myMethod() {
+    System.out.println("Why Java?");
+    }
+}
---
+++ second.java
@@ -0,0 +1,8 @@
+public class second {
+    public static void main(String[] args) {
+        main.myMethod();
+        main myObj = new main();
+        System.out.println(myObj.x);
+        System.out.println(myObj.y);
+    }
+}
